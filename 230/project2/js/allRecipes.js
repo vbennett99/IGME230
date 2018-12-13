dedd6f19068b8af89c782fc71341a921a342f9ef
Vincent Bennett <vbennett99@users.noreply.github.com>
2018-12-13 11:09:27 -0500

    LOTS OF JS
    many igme230 projects

---
+++ 230/project2/js/allRecipes.js
@@ -0,0 +1,773 @@
+let allRecipes = [];
+
+// ----- VEGETARIAN DISHES -----//
+
+let creamyHeartSoup =
+    {
+        name: "Creamy Heart Soup",
+        type: "vegetarian",
+        ingredient1: "radish",
+        ingredient2: "hydromelon",
+        ingredient3: "voltfruit",
+        ingredient4: "fresh milk",
+        ingredient5: "",
+        keyword: "broccoli & stilton soup"
+    };
+allRecipes.push(creamyHeartSoup);
+
+let pumpkinStew =
+    {
+        name: "Pumpkin Stew",
+        type: "vegetarian",
+        ingredient1: "fortified pumpkin",
+        ingredient2: "goat butter",
+        ingredient3: "fresh milk",
+        ingredient4: "tabantha wheat",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(pumpkinStew);
+
+let veggieCreamSoup =
+    {
+        name: "Veggie Cream Soup",
+        type: "vegetarian",
+        ingredient1: "carrot",
+        ingredient2: "pumpkin",
+        ingredient3: "fresh milk",
+        ingredient4: "rock salt",
+        ingredient5: "",
+        keyword: "split pea soup"
+    };
+allRecipes.push(veggieCreamSoup);
+
+let creamOfMushroomSoup =
+    {
+        name: "Cream of Mushroom Soup",
+        type: "vegetarian",
+        ingredient1: "mushroom",
+        ingredient2: "herb",
+        ingredient3: "fresh milk",
+        ingredient4: "rock salt",
+        ingredient5: "",
+        keyword: "hot and sour soup"
+    };
+allRecipes.push(creamOfMushroomSoup);
+
+let creamOfVegetableSoup =
+    {
+        name: "Cream of Vegetable Soup",
+        type: "vegetarian",
+        ingredient1: "radish",
+        ingredient2: "hydromelon",
+        ingredient3: "voltfruit",
+        ingredient4: "fresh milk",
+        ingredient5: "",
+        keyword: "broccoli & stilton soup"
+    };
+allRecipes.push(creamOfVegetableSoup);
+
+let carrotStew =
+    {
+        name: "Carrot Stew",
+        type: "vegetarian",
+        ingredient1: "carrot",
+        ingredient2: "goat butter",
+        ingredient3: "fresh milk",
+        ingredient4: "tabantha wheat",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(carrotStew);
+
+let vegetableRisotto =
+    {
+        name: "Vegetable Risotto",
+        type: "vegetarian",
+        ingredient1: "carrot",
+        ingredient2: "goat butter",
+        ingredient3: "hylian rice",
+        ingredient4: "rock salt",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(vegetableRisotto);
+
+let mushroomRisotto =
+    {
+        name: "Vegetable Risotto",
+        type: "vegetarian",
+        ingredient1: "mushroom",
+        ingredient2: "hylian rice",
+        ingredient3: "goat butter",
+        ingredient4: "rock salt",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(mushroomRisotto);
+
+let curryPilaf =
+    {
+        name: "Curry Pilaf",
+        type: "vegetarian",
+        ingredient1: "goat butter",
+        ingredient2: "goron spice",
+        ingredient3: "hylian rice",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(curryPilaf);
+
+let mushroomRiceBalls =
+    {
+        name: "Mushroom Rice Balls",
+        type: "vegetarian",
+        ingredient1: "hylian rice",
+        ingredient2: "mushroom",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(mushroomRiceBalls);
+
+let veggieRiceBalls =
+    {
+        name: "Veggie Rice Balls",
+        type: "vegetarian",
+        ingredient1: "hylian rice",
+        ingredient2: "herb",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(veggieRiceBalls);
+
+let curryRice =
+    {
+        name: "Curry Rice",
+        type: "vegetarian",
+        ingredient1: "goron spice",
+        ingredient2: "hylian rice",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: "kidney bean curry"
+    };
+allRecipes.push(curryRice);
+
+let friedEggAndRice =
+    {
+        name: "Fried Egg and Rice",
+        type: "vegetarian",
+        ingredient1: "bird egg",
+        ingredient2: "hylian rice",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(friedEggAndRice);
+
+let mushroomOmelette =
+    {
+        name: "Mushroom Omelette",
+        type: "vegetarian",
+        ingredient1: "mushroom",
+        ingredient2: "bird egg",
+        ingredient3: "goat butter",
+        ingredient4: "rock salt",
+        ingredient5: "",
+        keyword: "french omelette"
+    };
+allRecipes.push(mushroomOmelette);
+
+let vegetableOmelette =
+    {
+        name: "Vegetable Omelette",
+        type: "vegetarian",
+        ingredient1: "herb",
+        ingredient2: "bird egg",
+        ingredient3: "goat butter",
+        ingredient4: "rock salt",
+        ingredient5: "",
+        keyword: "omelette cake"
+    };
+allRecipes.push(vegetableOmelette);
+
+let vegetableCurry =
+    {
+        name: "Vegetable Curry",
+        type: "vegetarian",
+        ingredient1: "herb",
+        ingredient2: "goron spice",
+        ingredient3: "hylian rice",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: "kidney bean curry"
+    };
+allRecipes.push(vegetableCurry);
+
+let fragrantMushroomSaute =
+    {
+        name: "Fragrant Mushroom Saute",
+        type: "vegetarian",
+        ingredient1: "mushroom",
+        ingredient2: "goron spice",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: "mushroom & chestnut rotolo"
+    };
+allRecipes.push(fragrantMushroomSaute);
+
+let herbSaute =
+    {
+        name: "Herb Saute",
+        type: "vegetarian",
+        ingredient1: "herb",
+        ingredient2: "goron spice",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(herbSaute);
+
+let saltGrilledGreens =
+    {
+        name: "Salt-Grilled Greens",
+        type: "vegetarian",
+        ingredient1: "herb",
+        ingredient2: "rock salt",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(saltGrilledGreens);
+
+let steamedMushrooms =
+    {
+        name: "Steamed Mushrooms",
+        type: "vegetarian",
+        ingredient1: "herb",
+        ingredient2: "mushroom",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(steamedMushrooms);
+
+let mushroomSkewer =
+    {
+        name: "Mushroom Skewer",
+        type: "vegetarian",
+        ingredient1: "mushroom",
+        ingredient2: "",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(mushroomSkewer);
+
+let copiousMushroomSkewers =
+    {
+        name: "Copious Mushroom Skewers",
+        type: "vegetarian",
+        ingredient1: "mushroom",
+        ingredient2: "mushroom",
+        ingredient3: "mushroom",
+        ingredient4: "mushroom",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(copiousMushroomSkewers);
+
+let friedWildGreens =
+    {
+        name: "Fried Wild Greens",
+        type: "vegetarian",
+        ingredient1: "herb",
+        ingredient2: "",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(friedWildGreens);
+
+let copiousFriedWildGreens =
+    {
+        name: "Copious Fried Wild Greens",
+        type: "vegetarian",
+        ingredient1: "herb",
+        ingredient2: "herb",
+        ingredient3: "herb",
+        ingredient4: "herb",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(copiousFriedWildGreens);
+
+let omelette =
+    {
+        name: "Omelette",
+        type: "vegetarian",
+        ingredient1: "bird egg",
+        ingredient2: "",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: "french omelette"
+    };
+allRecipes.push(omelette);
+
+// ----- SEAFOOD DISHES -----
+
+let clamChowder =
+    {
+        name: "clamChowder",
+        type: "seafood",
+        ingredient1: "hearty blueshell snail",
+        ingredient2: "goat butter",
+        ingredient3: "fresh milk",
+        ingredient4: "tabantha wheat",
+        ingredient5: "",
+        keyword: "clam chowder"
+    };
+allRecipes.push(clamChowder);
+
+let creamySeafoodSoup =
+    {
+        name: "Creamy Seafood Soup",
+        type: "seafood",
+        ingredient1: "seafood",
+        ingredient2: "herb",
+        ingredient3: "fresh milk",
+        ingredient4: "rock salt",
+        ingredient5: "",
+        keyword: "prawn & fennel bisque"
+    };
+allRecipes.push(creamySeafoodSoup);
+
+let seafoodCurry =
+    {
+        name: "Seafood Curry",
+        type: "seafood",
+        ingredient1: "hearty blueshell snail",
+        ingredient2: "porgy",
+        ingredient3: "hylian rice",
+        ingredient4: "goron spice",
+        ingredient5: "",
+        keyword: "thai green curry"
+    };
+allRecipes.push(seafoodCurry);
+
+let salmonRisotto =
+    {
+        name: "Salmon Risotto",
+        type: "seafood",
+        ingredient1: "hearty salmon",
+        ingredient2: "goat butter",
+        ingredient3: "rock salt",
+        ingredient4: "hylian rice",
+        ingredient5: "",
+        keyword: "salmon prawn risotto"
+    };
+allRecipes.push(salmonRisotto);
+
+let crabRisotto =
+    {
+        name: "Crab Risotto",
+        type: "seafood",
+        ingredient1: "crab",
+        ingredient2: "rock salt",
+        ingredient3: "hylian rice",
+        ingredient4: "goat butter",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(crabRisotto);
+
+let seafoodFriedRice =
+    {
+        name: "Seafood Fried Rice",
+        type: "seafood",
+        ingredient1: "hearty blueshell snail",
+        ingredient2: "porgy",
+        ingredient3: "rock salt",
+        ingredient4: "hylian rice",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(seafoodFriedRice);
+
+let seafoodPaella =
+    {
+        name: "Seafood Paella",
+        type: "seafood",
+        ingredient1: "hearty blueshell snail",
+        ingredient2: "porgy",
+        ingredient3: "rock salt",
+        ingredient4: "goat butter",
+        ingredient5: "hylian rice",
+        keyword: "roasted fennel and aubergine paella"
+    };
+allRecipes.push(seafoodPaella);
+
+let crabOmeletteWithRice =
+    {
+        name: "Crab Omelette with Rice",
+        type: "seafood",
+        ingredient1: "crab",
+        ingredient2: "bird egg",
+        ingredient3: "rock salt",
+        ingredient4: "hylian rice",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(crabOmeletteWithRice);
+
+let seafoodRiceBalls =
+    {
+        name: "Seafood Rice Balls",
+        type: "seafood",
+        ingredient1: "fish",
+        ingredient2: "hylian rice",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(seafoodRiceBalls);
+
+let fishPie =
+    {
+        name: "Fish Pie",
+        type: "seafood",
+        ingredient1: "seafood",
+        ingredient2: "rock salt",
+        ingredient3: "goat butter",
+        ingredient4: "tabantha wheat",
+        ingredient5: "",
+        keyword: "fish pie"
+    };
+allRecipes.push(fishPie);
+
+let salmonMeuniere =
+    {
+        name: "Salmon Meuniere",
+        type: "seafood",
+        ingredient1: "hearty salmon",
+        ingredient2: "goat butter",
+        ingredient3: "tabantha wheat",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(salmonMeuniere);
+
+let porgyMeuniere =
+    {
+        name: "Porgy Meuniere",
+        type: "seafood",
+        ingredient1: "porgy",
+        ingredient2: "goat butter",
+        ingredient3: "tabantha wheat",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(porgyMeuniere);
+
+let seafoodMeuniere =
+    {
+        name: "Seafood Meuniere",
+        type: "seafood",
+        ingredient1: "fish",
+        ingredient2: "goat butter",
+        ingredient3: "tabantha wheat",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(seafoodMeuniere);
+
+let glazedSeafood =
+    {
+        name: "Glazed Seafood",
+        type: "seafood",
+        ingredient1: "seafood",
+        ingredient2: "courser bee honey",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: "honey teriyaki salmon"
+    };
+allRecipes.push(glazedSeafood);
+
+let crabStirFry =
+    {
+        name: "Crab Stir-Fry",
+        type: "seafood",
+        ingredient1: "crab",
+        ingredient2: "goron spice",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(crabStirFry)
+
+let saltGrilledCrab =
+    {
+        name: "Salt-Grilled Crab",
+        type: "seafood",
+        ingredient1: "crab",
+        ingredient2: "rock ralt",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(saltGrilledCrab);
+
+let saltGrilledFish =
+    {
+        name: "Salt-Grilled Fish",
+        type: "seafood",
+        ingredient1: "fish",
+        ingredient2: "rock salt",
+        ingredient3: "",
+        ingredient4: "",
+        ingredient5: "",
+        keyword: "escovitch fish"
+    };
+allRecipes.push(saltGrilledFish);
+
+let copiousFishSkewers =
+    {
+        name: "Copious Fish Skewers",
+        type: "seafood",
+        ingredient1: "fish",
+        ingredient2: "fish",
+        ingredient3: "fish",
+        ingredient4: "fish",
+        ingredient5: "",
+        keyword: ""
+    };
+allRecipes.push(copiousFishSkewers);
+
+// ----- MEAT DISHES -----
+let meatStew =
+    {
+    name: "Meat Stew",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "fresh milk",
+    ingredient3: "goat butter",
+    ingredient4: "tabantha wheat",
+    ingredient5: "drumstick",
+    keyword: "oxtail with broad beans"
+};
+allRecipes.push(meatStew);
+
+let creamyMeatSoup =
+    {
+    name: "Creamy Meat Soup",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "herb",
+    ingredient3: "fresh milk",
+    ingredient4: "rock salt",
+    ingredient5: "",
+    keyword: "thai green curry"
+};
+allRecipes.push(creamyMeatSoup);
+
+let meatCurry =
+    {
+    name: "Meat Curry",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "goron spice",
+    ingredient3: "hylian rice",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: "Massaman Beef curry"
+};
+allRecipes.push(meatCurry);
+
+let poultryCurry =
+    {
+    name: "Poultry Curry",
+    type: "meat",
+    ingredient1: "drumstick",
+    ingredient2: "goron spice",
+    ingredient3: "hylian rice",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: "Katsu Chicken curry"
+};
+allRecipes.push(poultryCurry);
+
+let meatyRiceBalls =
+    {
+    name: "Meaty Rice Balls",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "hylian rice",
+    ingredient3: "",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: ""
+};
+allRecipes.push(meatyRiceBalls);
+
+let meatStuffedPumpkins =
+    {
+    name: "Meat-Stuffed Pumpkins",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "pumpkin",
+    ingredient3: "",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: ""
+};
+allRecipes.push(meatStuffedPumpkins);
+
+let meatPie =
+{
+    name: "Meat Pie",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "goat butter",
+    ingredient3: "rock salt",
+    ingredient4: "tabantha wheat",
+    ingredient5: "",
+keyword: "Minced Beef Pie"
+};
+allRecipes.push(meatPie);
+
+let poultryPilaf =
+{
+    name: "Poultry Pilaf",
+    type: "meat",
+    ingredient1: "drumstick",
+    ingredient2: "bird egg",
+    ingredient3: "goat butter",
+    ingredient4: "hylian rice",
+    ingredient5: "",
+    keyword: "Teriyaki Chicken Casserole"
+};
+allRecipes.push(poultryPilaf);
+
+let meatAndRiceBowl =
+{
+    name: "Meat and Rice Bowl",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "rock salt",
+    ingredient3: "hylian rice",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: "Jerk chicken with rice & peas"
+};
+allRecipes.push(meatAndRiceBowl);
+
+let glazedMeat =
+{
+    name: "Glazed Meat",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "courser bee honey",
+    ingredient3: "",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: "General Tso's Chicken"
+};
+allRecipes.push(glazedMeat);
+
+let spicedMeatSkewer =
+{
+    name: "Spiced Meat Skewer",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "goron spice",
+    ingredient3: "",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: ""
+};
+allRecipes.push(spicedMeatSkewer);
+
+let saltGrilledMeat =
+{
+    name: "Salt-Grilled Meat",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "drumstick",
+    ingredient3: "rock salt",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: "Vietnamese Grilled Pork"
+};
+allRecipes.push(saltGrilledMeat);
+
+let pepperSteak =
+{
+    name: "Pepper Steak",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "spicy pepper",
+    ingredient3: "",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: "Steak Diane"
+};
+allRecipes.push(pepperSteak);
+
+let steamedMeat =
+{
+    name: "Steamed Meat",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "herb",
+    ingredient3: "",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: "Duck Confit"
+};
+allRecipes.push(steamedMeat);
+
+let meatAndMushroomSkewer =
+{
+    name: "Meat and Mushroom Skewer",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "mushroom",
+    ingredient3: "",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: ""
+};
+allRecipes.push(meatAndMushroomSkewer);
+
+let meatSkewer =
+{
+    name: "Meat Skewer",
+    type: "meat",
+    ingredient1: "meat",
+    ingredient2: "",
+    ingredient3: "",
+    ingredient4: "",
+    ingredient5: "",
+    keyword: ""
+};
+allRecipes.push(meatSkewer);
--- 230/project2/js/main.js
+++ 230/project2/js/main.js
@@ -1,6 +1,184 @@
-window.onload = (e) => {document.querySelector("#search").onclick = getData};
+window.onload = (e) => {document.querySelector("#search").onclick = findInGameMeal};
 
-let displayTerm = "";
+let currentResult;
+let youtubeLink;
+
+function findInGameMeal()
+{
+    let searchTerm = document.querySelector("#searchterm").value;   
+    searchTerm = searchTerm.trim();
+    searchTerm = searchTerm.toLowerCase();
+    
+    //Get all matching results
+    let results = [];
+    
+    //Getting result limit
+    let limit = document.querySelector("#limit").value;
+    let displayLimit;
+    if(limit == "x")
+    {
+        displayLimit = allRecipes.length; //Display all
+    }
+    else
+    {
+        displayLimit = limit; //Display specified number
+    }
+    
+    let resultCounter = 0;
+    
+    //Getting results
+    for(let i = 0; i < allRecipes.length; i++)
+    {
+        if(resultCounter != displayLimit)
+        {
+               //If the search term is the recipe's name
+            if(allRecipes[i].name.includes(searchTerm))
+            {
+                results.push(allRecipes[i]);
+                //console.log("Found name match, adding to results");
+                resultCounter++;
+            }
+         
+            //If the type of meal it is is the search term
+            if(allRecipes[i].type == searchTerm)
+            {
+                results.push(allRecipes[i]);   
+                //console.log("Found type match, adding to results");
+                resultCounter++;
+            }
+            //If the recipe's ingredients includes the search term, ex: 'honey' in 'courser bee honey'
+            else if(allRecipes[i].ingredient1.includes(searchTerm))
+            {
+                results.push(allRecipes[i]);
+                //console.log("Found ingredient match, adding to results");
+                resultCounter++;
+            }
+            else if(allRecipes[i].ingredient2.includes(searchTerm))
+            {
+                results.push(allRecipes[i]);
+                //console.log("Found ingredient match, adding to results");
+                resultCounter++;
+            }
+            else if(allRecipes[i].ingredient3.includes(searchTerm))
+            {
+                results.push(allRecipes[i]);
+                //console.log("Found ingredient match, adding to results");
+                resultCounter++;
+            }
+            else if(allRecipes[i].ingredient4.includes(searchTerm))
+            {
+                results.push(allRecipes[i]);
+                //////console.log("Found ingredient match, adding to results");
+                resultCounter++;
+            }
+            else if(allRecipes[i].ingredient5.includes(searchTerm))
+            {
+                results.push(allRecipes[i]);
+                ////console.log("Found ingredient match, adding to results");
+                resultCounter++;
+            }
+        }
+    }
+    
+    //console.log("Results: " + results.length);
+    
+    //Cutting down results list based on max number of ingredients
+    let trimmedResults = [];
+    
+    //See what the ingredient max is
+    let ingredientMax = document.querySelector("#ingredientLimit").value;
+    //console.log("Ingredient limit: " + ingredientMax);
+    
+    for(let i = 0; i < results.length; i++)
+    {
+        let result = results[i]; //Get a result
+        
+        let numIngredients = 1; //Ingredient counter, there will always be at least one
+        
+        if(result.ingredient2 != "")
+        {
+            numIngredients++;   
+        }
+        if(result.ingredient3 != "")
+        {
+            numIngredients++;
+        }
+        if(result.ingredient4 != "")
+        {
+            numIngredients++;
+        }
+        if(result.ingredient5 != "")
+        {
+            numIngredients++;
+        }
+        
+        if(numIngredients <= ingredientMax) //If the number of ingredients this recipe has is less than or equal to the max then add it to trimmedResults
+        {
+            trimmedResults.push(result);
+        }
+    }
+    
+    //console.log("Trimmed Results Length: " + trimmedResults.length);
+    
+    //If there aren't any results after trimming
+    if(trimmedResults.length == 0)
+    {
+        document.querySelector("#displaytext").innerHTML = `No results found for '${searchTerm}.' Try searching something else!`;
+        return;
+    }
+    else
+    {
+        document.querySelector("#displaytext").innerHTML = `Showing results for '${searchTerm}'`;   
+    }
+    
+    //Displaying our results
+    for(let i = 0; i < trimmedResults.length; i++)
+    {
+        currentResult = trimmedResults[i];
+        
+        let display = `<div class="result"><h4>${trimmedResults[i].name}</h4><ul>`;
+        
+        display += `<li>${trimmedResults[i].ingredient1}</li>`;
+        if(trimmedResults[i].ingredient2 != "")
+        {
+            display += `<li>${trimmedResults[i].ingredient2}</li>`;
+        }
+        if(trimmedResults[i].ingredient3 != "")
+        {
+            display += `<li>${trimmedResults[i].ingredient3}</li>`;
+        }
+        if(trimmedResults[i].ingredient4 != "")
+        {
+            display += `<li>${trimmedResults[i].ingredient4}</li>`;
+        }
+        if(trimmedResults[i].ingredient5 != "")
+        {
+            display += `<li>${trimmedResults[i].ingredient5}</li>`;
+        }
+        
+        display += `</ul><p class="dropdown">`;
+        
+        //If there's a keyword aka a recipe to go along
+        /*if(trimmedResults[i].keyword != "")
+        {
+            let link = getData();
+            display += `<a target="_blank" href="">`;
+            display += `Click here to see a real recipe!`;
+            display += `</a>`
+            
+        }
+        else
+        {*/
+            display += `There is no real recipe to go along with this meal.`;
+        //}
+                    
+                //<p class="dropdown">Click to see a real recipe!</p>
+            //</div>
+        display += `</p></div><br>`;
+        
+        document.querySelector("#content").innerHTML += display;
+    }
+}
 
 function getData()
 {
@@ -13,8 +191,6 @@ function getData()
     let term = document.querySelector("#searchterm").value;
     displayTerm = term;
     
-    term = term.trim();
-    
     term = encodeURIComponent(term);
     
     //If the search has nothing in it, stop executing the function
@@ -22,32 +198,34 @@ function getData()
     
     url += "search.php?s=" + term;
     
-    document.querySelector("#content").innerHTML = "Searching for recipes with " + displayTerm;
-    
     console.log(url);
-    //console.log(jQuery);
-    //console.log($);
+    console.log(jQuery);
+    console.log($);
     
-    /*$.ajax ({
+    $.ajax ({
         dataType: "json",  
         url: url,
         data: null,
         success: jsonLoaded
-    })*/
+    });
 }
 
 function jsonLoaded(obj)
 {
-    console.log("obj = " + obj);
     console.log("obj stringified = " + JSON.stringify(obj));
+    
+    let stringified = JSON.stringify(obj);
     
     if(!obj.data || obj.data.length == 0)
     {
-        document.querySelector("#content").innerHTML = `No results found for '${displayTerm}'`;
+        document.querySelector("#content").innerHTML = `<p>No results found for '${displayTerm}'</p>`;
         $("#content").fadeIn(500);
         return;
     }
-    
+    else
+    {
+        console.log("There is data.");   
+    }
     
     
 }
